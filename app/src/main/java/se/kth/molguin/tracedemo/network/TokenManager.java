fd00c13bf2fab9c5895d4677eff0d37f423cba85
Manuel Osvaldo Olguín <molguin@kth.se>
2018-03-29 17:04:42 +0200

    implementing get token routine

--- app/src/main/java/se/kth/molguin/tracedemo/network/TokenManager.java
+++ app/src/main/java/se/kth/molguin/tracedemo/network/TokenManager.java
@@ -1,4 +1,25 @@
 package se.kth.molguin.tracedemo.network;
 
 class TokenManager {
+
+    private final Object lock = new Object();
+    private boolean hasToken;
+
+    TokenManager() {
+        hasToken = true;
+    }
+
+    void getToken() {
+        synchronized (lock) {
+            if (!hasToken) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException ignored) {
+                }
+            }
+            
+            hasToken = false;
+        }
+    }
+
 }
