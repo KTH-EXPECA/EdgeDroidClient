3989d2620fc7700124e4150c30a545f65c5757fd
Manuel Osvaldo Olguín <molguin@kth.se>
2018-04-05 14:15:05 +0200

    extract constants

--- app/src/main/java/se/kth/molguin/tracedemo/Constants.java
+++ app/src/main/java/se/kth/molguin/tracedemo/Constants.java
@@ -1,14 +1,14 @@
 package se.kth.molguin.tracedemo;
 
 public class Constants {
-    static final String CONNECT_TXT = "Connect";
-    static final String DISCONNECT_TXT = "Disconnect";
-    static final String STATUS_DISCONNECTED_FMT = "Disconnected";
-    static final String STATUS_CONNECTING_FMT = "Connecting to %s...";
-    static final String STATUS_CONNECTED_FMT = "Connected to %s";
-    static final String STATUS_STREAMING_FMT = "Connected and streaming to %s";
-    static final String STATUS_STREAM_DONE_FMT = "Streaming done. Disconnecting...";
-    static final String STATUS_DISCONNECTING_FMT = "Closing connectiong to %s...";
-    static final String PREFS_ADDR = "GABRIEL_ADDR";
-    static final int FPS = 15;
+    public static final String CONNECT_TXT = "Connect";
+    public static final String DISCONNECT_TXT = "Disconnect";
+    public static final String STATUS_DISCONNECTED_FMT = "Disconnected";
+    public static final String STATUS_CONNECTING_FMT = "Connecting to %s...";
+    public static final String STATUS_CONNECTED_FMT = "Connected to %s";
+    public static final String STATUS_STREAMING_FMT = "Connected and streaming to %s";
+    public static final String STATUS_STREAM_DONE_FMT = "Streaming done. Disconnecting...";
+    public static final String STATUS_DISCONNECTING_FMT = "Closing connectiong to %s...";
+    public static final String PREFS_ADDR = "GABRIEL_ADDR";
+    public static final int FPS = 15;
 }
