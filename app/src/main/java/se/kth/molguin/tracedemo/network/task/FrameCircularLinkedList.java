038a58fec0ec30aaa52b2820a0b226eeb78e4d77
Manuel Osvaldo Olguín <molguin@kth.se>
2018-04-05 12:36:50 +0200

    extend functionality of circular linked list

--- app/src/main/java/se/kth/molguin/tracedemo/network/task/FrameCircularLinkedList.java
+++ app/src/main/java/se/kth/molguin/tracedemo/network/task/FrameCircularLinkedList.java
@@ -29,6 +29,33 @@ public class FrameCircularLinkedList implements Iterable<byte[]> {
         }
     }
 
+    public byte[] getCurrent() {
+        if (this.head == null) return null;
+        return head.data;
+    }
+
+    public void stepForward() {
+        if (this.head == null) return;
+        this.head = this.head.next;
+    }
+
+    public void stepBackward() {
+        if (this.head == null) return;
+        this.head = this.head.previous;
+    }
+
+    public void rewind(int count) {
+        if (count < 0) return;
+        for (int i = 0; i < count; i++)
+            this.stepBackward();
+    }
+
+    public void fastForward(int count) {
+        if (count < 0) return;
+        for (int i = 0; i < count; i++)
+            this.stepForward();
+    }
+
     @NonNull
     @Override
     public Iterator<byte[]> iterator() {
