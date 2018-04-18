bd551cddbce697e41c09cc5142199fe12489f097
Manuel Osvaldo Olguín <molguin@kth.se>
2018-04-18 11:37:06 +0200

    update some constants

--- app/src/main/java/se/kth/molguin/tracedemo/Constants.java
+++ app/src/main/java/se/kth/molguin/tracedemo/Constants.java
@@ -25,5 +25,5 @@ public class Constants {
     public static final int REWIND_SECONDS = 5;
     public static final int MIN_MISTAKE_COUNT = 10;
 
-    public static final float MAX_NTP_DISPERSION = 500.0f;
+    public static final float MAX_NTP_DISPERSION = 10.0f;
 }
