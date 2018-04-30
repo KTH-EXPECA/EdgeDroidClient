c6fecfca06dd5b69d1199f76ef16f3321d628895
Manuel Osvaldo Olguín <molguin@kth.se>
2018-04-30 13:02:44 +0200

    Experiment class for handling stats and easier serialization

---
+++ app/src/main/java/se/kth/molguin/tracedemo/network/gabriel/Experiment.java
@@ -0,0 +1,148 @@
+package se.kth.molguin.tracedemo.network.gabriel;
+
+import com.instacart.library.truetime.TrueTimeRx;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import se.kth.molguin.tracedemo.StatBackendConstants;
+
+public abstract class Experiment {
+    private static final int STAT_WINDOW_SZ = 15;
+
+    public static class Run {
+        Date init;
+        Date finish;
+        HashSet<Integer> feedback_frames;
+        LinkedList<Frame> frames;
+        DescriptiveStatistics rtt;
+        boolean success;
+
+        public Run()
+        {
+            this.init = null;
+            this.finish = null;
+            this.success = false;
+
+            this.feedback_frames = new HashSet<>();
+            this.frames = new LinkedList<>();
+            this.rtt = new DescriptiveStatistics(STAT_WINDOW_SZ);
+        }
+
+        public void init()
+        {
+            this.init = TrueTimeRx.now();
+        }
+
+        public void finish()
+        {
+            this.finish = TrueTimeRx.now();
+        }
+
+        public void setSuccess(boolean success) {
+            this.success = success;
+        }
+
+        public void registerFrame(int frame_id, Date sent, Date recv, boolean feedback)
+        {
+            Frame f = new Frame(frame_id, sent, recv);
+            this.frames.add(f);
+
+            if (feedback)
+                this.feedback_frames.add(frame_id);
+
+            this.rtt.addValue(f.getRTT());
+        }
+
+        public double getRollingRTT()
+        {
+            return this.rtt.getMean();
+        }
+
+        long getInitTimestamp()
+        {
+            Calendar c = Calendar.getInstance();
+            c.setTime(this.init);
+            return c.getTimeInMillis();
+        }
+
+        long getFinishTimestamp()
+        {
+            Calendar c = Calendar.getInstance();
+            c.setTime(this.finish);
+            return c.getTimeInMillis();
+        }
+
+        public JSONObject toJSON() throws JSONException
+        {
+            JSONObject repr = new JSONObject();
+
+            repr.put(StatBackendConstants.FIELD_RUNBEGIN, this.getInitTimestamp());
+            repr.put(StatBackendConstants.FIELD_RUNEND, this.getFinishTimestamp());
+            repr.put(StatBackendConstants.FIELD_RUNSUCCESS, this.success);
+
+            JSONArray json_frames = new JSONArray();
+            for (Frame f: this.frames)
+            {
+                JSONObject f_json = f.toJSON();
+                if (this.feedback_frames.contains(f.id))
+                    f_json.put(StatBackendConstants.FRAMEFIELD_FEEDBACK, true);
+                else f_json.put(StatBackendConstants.FRAMEFIELD_FEEDBACK, false);
+
+                json_frames.put(f_json);
+            }
+
+            repr.put(StatBackendConstants.FIELD_FRAMELIST, json_frames);
+
+            return repr;
+        }
+    }
+
+    private static class Frame {
+        int id;
+        Date sent;
+        Date recv;
+
+        Frame(int id, Date sent, Date recv)
+        {
+            this.id = id;
+            this.sent = sent;
+            this.recv = recv;
+        }
+
+        long getRTT()
+        {
+            Calendar c = Calendar.getInstance();
+            c.setTime(this.sent);
+            long s = c.getTimeInMillis();
+
+            c.setTime(this.recv);
+            long r = c.getTimeInMillis();
+
+            return r - s;
+        }
+
+        JSONObject toJSON() throws JSONException {
+            JSONObject repr = new JSONObject();
+            repr.put(StatBackendConstants.FRAMEFIELD_ID, this.id);
+
+            Calendar c = Calendar.getInstance();
+
+            c.setTime(this.sent);
+            repr.put(StatBackendConstants.FRAMEFIELD_SENT, c.getTimeInMillis());
+
+            c.setTime(this.recv);
+            repr.put(StatBackendConstants.FRAMEFIELD_RECV, c.getTimeInMillis());
+
+            return repr;
+        }
+    }
+}
