c45065164f11da0793907377a3d752edd98f282d
Manuel Osvaldo Olguín <molguin@kth.se>
2018-05-03 14:24:28 +0200

    multiple requests to NTP to smooth out errors

--- app/src/main/java/se/kth/molguin/tracedemo/synchronization/NTPClient.java
+++ app/src/main/java/se/kth/molguin/tracedemo/synchronization/NTPClient.java
@@ -12,6 +12,7 @@ with the Apache Commons Net software.
 
 import android.util.Log;
 
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import org.apache.commons.net.ntp.NTPUDPClient;
 import org.apache.commons.net.ntp.TimeInfo;
 
@@ -19,12 +20,14 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
-
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import static java.lang.System.exit;
+
 public class NTPClient implements AutoCloseable {
 
+    private static final int NTP_POLL_COUNT = 11;
     private static final int NTP_TIMEOUT = 100;
     private static final String LOG_TAG = "NTPClient";
     private ReadWriteLock lock;
@@ -32,30 +35,12 @@ public class NTPClient implements AutoCloseable {
     private InetAddress hostAddr;
     NTPUDPClient ntpUdpClient;
 
-    private TimeInfo timeInfo;
+    private long mean_offset;
+    private long mean_delay;
+    private long offset_err;
+    private long delay_err;
 
-    public void pollNtpServer() {
-        try {
-            TimeInfo ti = ntpUdpClient.getTime(hostAddr);
-            ti.computeDetails();
-
-            this.setTimeInfo(ti);
-            Log.i(LOG_TAG, "Polled " + this.hostAddr.toString());
-            Log.i(LOG_TAG, "Local time: " + System.currentTimeMillis());
-            Log.i(LOG_TAG, "Server time: " + this.currentTimeMillis());
-            Log.i(LOG_TAG, String.format(
-                    "Offset: %d\tDelay: %d",
-                    ti.getOffset(),
-                    ti.getDelay()
-            ));
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-            this.close();
-        }
-    }
-
+    private boolean sync;
 
     public NTPClient(String host) throws UnknownHostException, SocketException {
         this.hostAddr = InetAddress.getByName(host);
@@ -63,36 +48,76 @@ public class NTPClient implements AutoCloseable {
         this.ntpUdpClient.setDefaultTimeout(10000);
         this.ntpUdpClient.open();
         this.ntpUdpClient.setSoTimeout(NTP_TIMEOUT);
-        this.timeInfo = null;
-
+        this.sync = false;
         this.lock = new ReentrantReadWriteLock();
 
         this.pollNtpServer();
     }
 
-    /**
-     * Get the value of timeInfo
-     *
-     * @return the value of timeInfo
-     */
-    public TimeInfo getTimeInfo() {
-        this.lock.readLock().lock();
-        if (null == this.timeInfo) {
-            this.lock.readLock().unlock();
-            this.pollNtpServer();
-            this.lock.readLock().lock();
+    public void pollNtpServer() {
+        SummaryStatistics offsets = new SummaryStatistics();
+        SummaryStatistics delays = new SummaryStatistics();
+        try {
+            for (int i = 0; i < NTP_POLL_COUNT; i++)
+            {
+                TimeInfo ti = ntpUdpClient.getTime(hostAddr);
+                ti.computeDetails();
+
+                offsets.addValue(ti.getOffset());
+                delays.addValue(ti.getDelay());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            this.close();
+            exit(-1);
         }
 
-        TimeInfo result = this.timeInfo;
-        this.lock.readLock().unlock();
+        this.lock.writeLock().lock();
+        this.mean_offset = Math.round(offsets.getMean());
+        this.mean_delay = Math.round(delays.getMean());
+        this.offset_err = Math.round(offsets.getStandardDeviation());
+        this.delay_err = Math.round(delays.getStandardDeviation());
+        this.sync = true;
+        this.lock.writeLock().lock();
 
+        Log.i(LOG_TAG, "Polled " + this.hostAddr.toString());
+        Log.i(LOG_TAG, "Local time: " + System.currentTimeMillis());
+        Log.i(LOG_TAG, "Server time: " + this.currentTimeMillis());
+        Log.i(LOG_TAG, String.format(
+                "Offset: %f (+- %f) ms\tDelay: %f (+- %f) ms",
+                offsets.getMean(),
+                offsets.getStandardDeviation(),
+                delays.getMax(),
+                delays.getStandardDeviation()
+        ));
+    }
+
+    public long getMeanOffset() {
+        this.lock.readLock().lock();
+        long result = this.mean_offset;
+        this.lock.readLock().unlock();
         return result;
     }
 
-    private void setTimeInfo(TimeInfo timeInfo) {
-        this.lock.writeLock().lock();
-        this.timeInfo = timeInfo;
-        this.lock.writeLock().unlock();
+    public long getMeanDelay() {
+        this.lock.readLock().lock();
+        long result = this.mean_delay;
+        this.lock.readLock().unlock();
+        return result;
+    }
+
+    public long getOffsetError() {
+        this.lock.readLock().lock();
+        long result = this.offset_err;
+        this.lock.readLock().unlock();
+        return result;
+    }
+
+    public long getDelayError() {
+        this.lock.readLock().lock();
+        long result = this.delay_err;
+        this.lock.readLock().unlock();
+        return result;
     }
 
     /**
@@ -103,7 +128,7 @@ public class NTPClient implements AutoCloseable {
      */
     public long currentTimeMillis() {
         this.lock.readLock().lock();
-        if (null == this.timeInfo) {
+        if (!this.sync) {
             this.lock.readLock().unlock();
             this.pollNtpServer();
             this.lock.readLock().lock();
@@ -111,7 +136,7 @@ public class NTPClient implements AutoCloseable {
 
         //long diff = System.currentTimeMillis() - this.timeInfoSetLocalTime;
         //long result = timeInfo.getMessage().getReceiveTimeStamp().getTime() + diff;
-        long result = System.currentTimeMillis() + timeInfo.getOffset();
+        long result = System.currentTimeMillis() + this.mean_offset;
         this.lock.readLock().unlock();
 
         return result;
@@ -124,6 +149,7 @@ public class NTPClient implements AutoCloseable {
             ntpUdpClient.close();
             ntpUdpClient = null;
         }
+        this.sync = false;
         this.lock.writeLock().unlock();
     }
 
