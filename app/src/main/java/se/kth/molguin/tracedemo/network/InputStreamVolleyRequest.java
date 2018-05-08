6fd1a1af05f5110f3561f9ff4729702349d83fc2
Manuel Osvaldo Olguín <molguin@kth.se>
2018-05-08 18:54:30 +0200

    change volley timeout and retries

--- app/src/main/java/se/kth/molguin/tracedemo/network/InputStreamVolleyRequest.java
+++ app/src/main/java/se/kth/molguin/tracedemo/network/InputStreamVolleyRequest.java
@@ -1,5 +1,6 @@
 package se.kth.molguin.tracedemo.network;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -14,6 +15,9 @@ public class InputStreamVolleyRequest extends Request<byte[]> {
     public Map<String, String> responseHeaders;
     private Map<String, String> mParams;
 
+    private final static int TIMEOUT_MS = 500;
+    private final static int MAX_RETRIES = 20;
+
     public InputStreamVolleyRequest(int method, String mUrl, Response.Listener<byte[]> listener,
                                     Response.ErrorListener errorListener, HashMap<String, String> params) {
 
@@ -22,6 +26,10 @@ public class InputStreamVolleyRequest extends Request<byte[]> {
         setShouldCache(false);
         mListener = listener;
         mParams = params;
+
+        super.setRetryPolicy(new DefaultRetryPolicy(
+                TIMEOUT_MS, MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
     }
 
     @Override
