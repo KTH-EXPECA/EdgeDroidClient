ab24c25fbed5452c82378b889af0bcd9b44ff907
Manuel Osvaldo Olguín <molguin@kth.se>
2018-05-25 13:59:20 +0200

    refactor TokenManager to use semaphores for token count handling and locking

--- app/src/main/java/se/kth/molguin/tracedemo/network/gabriel/TokenManager.java
+++ app/src/main/java/se/kth/molguin/tracedemo/network/gabriel/TokenManager.java
@@ -1,38 +1,43 @@
 package se.kth.molguin.tracedemo.network.gabriel;
 
+import java.util.concurrent.Semaphore;
+
 public class TokenManager {
 
     private static TokenManager instance = null;
+    private final static int DEFAULT_TOKEN_COUNT = 1;
 
-    private static final Object lock = new Object();
-    private boolean hasToken;
+    //    private static final Object lock = new Object();
+    private Semaphore token_pool;
+//    private boolean hasToken;
 
-    public static TokenManager getInstance() {
-        synchronized (lock){
-            if (instance == null)
-                instance = new TokenManager();
-            return instance;
-        }
+    public synchronized static TokenManager getInstance() {
+        if (instance == null)
+            instance = new TokenManager();
+        return instance;
     }
 
     private TokenManager() {
-        hasToken = true;
+        // hasToken = true;
+        this.token_pool = new Semaphore(DEFAULT_TOKEN_COUNT, true);
     }
 
     public void getToken() throws InterruptedException {
-        synchronized (lock) {
-            while (!hasToken) {
-                lock.wait();
-            }
-            hasToken = false;
-        }
+//        synchronized (lock) {
+//            while (!hasToken) {
+//                lock.wait();
+//            }
+//            hasToken = false;
+//        }
+        this.token_pool.acquire();
     }
 
     public void putToken() {
-        synchronized (lock) {
-            if (!hasToken) hasToken = true;
-            lock.notify();
-        }
+//        synchronized (lock) {
+//            if (!hasToken) hasToken = true;
+//            lock.notify();
+//        }
+        this.token_pool.release();
     }
 
 }
