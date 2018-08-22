84aa2c633f505c9c9aa69806f0fd06618d333e7e
Manuel Osvaldo Olguín <molguin@kth.se>
2018-08-22 14:52:11 +0200

    Run uses asynclog and LiveData

--- app/src/main/java/se/kth/molguin/tracedemo/network/control/experiment/run/Run.java
+++ app/src/main/java/se/kth/molguin/tracedemo/network/control/experiment/run/Run.java
@@ -1,7 +1,7 @@
 package se.kth.molguin.tracedemo.network.control.experiment.run;
 
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,13 +14,12 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import se.kth.molguin.tracedemo.ApplicationStateUpdHandler;
-import se.kth.molguin.tracedemo.ModelState;
-import se.kth.molguin.tracedemo.network.task.ResultInputThread;
-import se.kth.molguin.tracedemo.network.task.VideoOutputThread;
+import se.kth.molguin.tracedemo.IntegratedAsyncLog;
 import se.kth.molguin.tracedemo.network.control.experiment.Config;
 import se.kth.molguin.tracedemo.network.control.experiment.Sockets;
 import se.kth.molguin.tracedemo.network.gabriel.TokenPool;
+import se.kth.molguin.tracedemo.network.task.ResultInputThread;
+import se.kth.molguin.tracedemo.network.task.VideoOutputThread;
 import se.kth.molguin.tracedemo.synchronization.INTPSync;
 
 public class Run {
@@ -39,7 +38,10 @@ public class Run {
 
     private final VideoOutputThread video_out;
     private final ResultInputThread result_in;
-    private final ModelState modelState;
+
+    private final IntegratedAsyncLog log;
+    private final MutableLiveData<byte[]> rtframe_feed;
+    private final MutableLiveData<byte[]> sentframe_feed;
 
     private boolean is_shutdown;
 
@@ -49,13 +51,22 @@ public class Run {
         }
     }
 
-    public Run(@NonNull final Config config, @NonNull final INTPSync ntp, @NonNull final ModelState modelState)
+    public Run(@NonNull final Config config,
+               @NonNull final INTPSync ntp,
+               @NonNull final IntegratedAsyncLog log,
+               @NonNull final MutableLiveData<byte[]> rtframe_feed,
+               @NonNull final MutableLiveData<byte[]> sentframe_feed)
             throws InterruptedException, ExecutionException, IOException {
+
         this.state_locks = new ReentrantReadWriteLock();
-        Log.i(LOG_TAG, "Initiating new Experiment Run");
+
+        this.log = log;
+        this.rtframe_feed = rtframe_feed;
+        this.sentframe_feed = sentframe_feed;
+
+        this.log.i(LOG_TAG, "Initiating new Experiment Run");
         this.config = config;
         this.ntp = ntp;
-        this.modelState = modelState;
 
         this.current_error_count = 0;
 
@@ -81,21 +92,19 @@ public class Run {
         this.checkRunning();
 
         this.stats.init();
-        ApplicationStateUpdHandler.infoMessage("Initializing experiment!");
-        Log.i(LOG_TAG, "Starting stream...");
+        this.log.i(LOG_TAG, "Initializing experiment!");
+        this.log.i(LOG_TAG, "Starting stream...");
         this.execs.execute(this.video_out);
         this.execs.execute(this.result_in);
-        ApplicationStateUpdHandler.infoMessage("Started streaming...");
+        this.log.i(LOG_TAG, "Started streaming...");
 
         // FIXME - wait for finish
     }
 
     public void finish() throws InterruptedException, IOException, RunException {
         this.checkRunning();
-        Log.i(LOG_TAG, "Experiment run ends");
-
-        ApplicationStateUpdHandler.infoMessage("Disconnecting...");
-        Log.i(LOG_TAG, "Disconnecting from CA backend...");
+        this.log.i(LOG_TAG, "Experiment run ends");
+        this.log.i(LOG_TAG, "Disconnecting from CA backend...");
         this.video_out.finish();
         this.result_in.stop();
 
@@ -103,7 +112,7 @@ public class Run {
             this.execs.shutdownNow();
 
         this.sockets.disconnect();
-        Log.i(LOG_TAG, "Disconnected from CA backend");
+        this.log.i(LOG_TAG, "Disconnected from CA backend");
         this.is_shutdown = true;
 
         // TODO: notify ControlClient
@@ -132,7 +141,7 @@ public class Run {
         this.state_locks.writeLock();
         try {
             this.current_error_count++;
-            Log.w(LOG_TAG, "Received error from backend, current count: " + this.current_error_count);
+            this.log.w(LOG_TAG, "Received error from backend, current count: " + this.current_error_count);
         } finally {
             this.state_locks.writeLock().unlock();
         }
