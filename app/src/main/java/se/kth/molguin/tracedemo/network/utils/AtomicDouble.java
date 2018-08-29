d2b23545fa6970f8bb8dcdf43d6d0d1d23f4ebf1
Manuel Osvaldo Olguín <molguin@kth.se>
2018-08-29 13:55:23 +0200

    added AtomicDouble

---
+++ app/src/main/java/se/kth/molguin/tracedemo/network/utils/AtomicDouble.java
@@ -0,0 +1,63 @@
+package se.kth.molguin.tracedemo.network.utils;
+
+// Utility class which represent an atomic double
+// uses AtomicInteger and bit conversion methods
+// as an internal backend
+
+// courtesy of user aioobe @ StackOverflow
+// https://stackoverflow.com/questions/5505460/java-is-there-no-atomicfloat-or-atomicdouble
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static java.lang.Double.doubleToLongBits;
+import static java.lang.Double.longBitsToDouble;
+
+class AtomicDouble extends Number {
+
+    private AtomicLong bits;
+
+    public AtomicDouble() {
+        this(0f);
+    }
+
+    public AtomicDouble(double initialValue) {
+        bits = new AtomicLong(doubleToLongBits(initialValue));
+    }
+
+    public final boolean compareAndSet(float expect, float update) {
+        return bits.compareAndSet(doubleToLongBits(expect), doubleToLongBits(update));
+    }
+
+    public final void set(float newValue) {
+        bits.set(doubleToLongBits(newValue));
+    }
+
+    public final double getAndSet(double newValue) {
+        return longBitsToDouble(bits.getAndSet(doubleToLongBits(newValue)));
+    }
+
+    public final boolean weakCompareAndSet(double expect, double update) {
+        return bits.weakCompareAndSet(doubleToLongBits(expect), doubleToLongBits(update));
+    }
+
+    public int intValue() {
+        return (int) get();
+    }
+
+    public long longValue() {
+        return (long) get();
+    }
+
+    public float floatValue() {
+        return (float) get();
+    }
+
+    public double doubleValue() {
+        return (double) floatValue();
+    }
+
+    public final double get() {
+        return longBitsToDouble(bits.get());
+    }
+
+}
